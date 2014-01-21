851955effa0ef6b0c8c84d84951095a00e58b734
ouyounm <MohammadOuyoun@Moo-Haha.local>
2014-01-21 11:43:44 -0600

    init commit
    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IosAndroid::Application.config.secret_key_base = '80f092da6806eefdb36c4ecede4158efa8ce2e6a313d17f61d17a3ce5f7c13a6369fd1b13b14ec0846916d42167b4e847d8417e3b4f4e2651549c0356ab23a61'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IosAndroid::Application.config.session_store :cookie_store, key: '_ios_android_session'
